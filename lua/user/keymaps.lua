759c4264add55777cabd1963c970e37afab66de1
Robin Kautz <git@bob3000.net>
2022-12-11 12:51:09 +0100

    set bufferline keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -37,8 +37,12 @@ keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
 keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
 
 -- Navigate buffers
-keymap("n", "<S-l>", ":bnext<CR>", opts)
-keymap("n", "<S-h>", ":bprevious<CR>", opts)
+keymap("n", "<S-l>", "<cmd>BufferLineCycleNext<CR>", opts)
+keymap("n", "<S-h>", "<cmd>BufferLineCyclePrev<CR>", opts)
+keymap("n", "<S-A-l>", "<cmd>BufferLineMoveNext<CR>", opts)
+keymap("n", "<S-A-h>", "<cmd>BufferLineMovePrev<CR>", opts)
+keymap("n", "<S-A-p>", "<cmd>BufferLineTogglePin<CR>", opts)
+keymap("n", "gB", "<cmd>BufferLinePick<CR>", opts)
 
 -- Clear highlights
 keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
