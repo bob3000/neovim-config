a4687bbcb4845caf49af3d884d6629a08d794048
Robin Kautz <git@bob3000.net>
2022-12-20 21:02:37 +0100

    configure isort and write-good

--- README.md
+++ README.md
@@ -48,6 +48,8 @@ Make sure to remove or move your current `nvim` directory
 git clone git@github.com:bob3000/neovim-config.git ~/.config/nvim
 ```
 
+
+
 Run `nvim` and wait for the plugins to be installed
 
 **NOTE** First time you will get an error just ignore them and press enter, it will say nvim-ts-context-commentstring is not installed but that is fine just close and reopen nvim and everything should be fine
@@ -67,7 +69,7 @@ Open `nvim` and enter the following:
 from inside neovim do:
 
 ```
-:MasonInstall black flake8 delve
+:MasonInstall isort black flake8 delve write-good
 ```
 
 ## Credits
--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -7,18 +7,23 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/code_actions
+local code_actions = null_ls.builtins.code_actions
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
 null_ls.setup {
   debug = false,
   sources = {
+    diagnostics.write_good.with { filetypes = { "markdown", "text" } },
     formatting.prettier.with {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
     },
     formatting.black.with { extra_args = { "--fast" } },
+    formatting.isort,
     formatting.stylua,
-    formatting.npm_groovy_lint,
     diagnostics.flake8,
+    code_actions.gitsigns,
+    formatting.npm_groovy_lint,
   },
 }
