d4c935d72bf2f3e926a36a8c63bbc966c509b4b6
Robin Kautz <git@bob3000.net>
2022-12-21 08:53:44 +0100

    remove write good

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -14,7 +14,6 @@ local code_actions = null_ls.builtins.code_actions
 null_ls.setup {
   debug = false,
   sources = {
-    diagnostics.write_good.with { filetypes = { "markdown", "text" } },
     formatting.prettier.with {
       extra_filetypes = { "toml" },
       extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
