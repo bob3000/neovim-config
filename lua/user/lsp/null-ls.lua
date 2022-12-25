6632c18182217e099e1c99c8ddd14df56f9a5659
Robin Kautz <git@bob3000.net>
2022-12-25 12:56:45 +0100

    configures cspell (disabled by default)

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-  return
+	return
 end
 
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -11,18 +11,22 @@ local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
 -- https://github.com/prettier-solidity/prettier-plugin-solidity
-null_ls.setup {
-  debug = false,
-  sources = {
-    formatting.prettier.with {
-      extra_filetypes = { "toml" },
-      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
-    },
-    formatting.black.with { extra_args = { "--fast" } },
-    formatting.isort,
-    formatting.stylua,
-    diagnostics.flake8,
-    code_actions.gitsigns,
-    formatting.npm_groovy_lint,
-  },
-}
+null_ls.setup({
+	debug = false,
+	sources = {
+		code_actions.cspell,
+		code_actions.gitsigns,
+		-- diagnostics.codespell,
+		-- diagnostics.cspell,
+		diagnostics.flake8,
+		formatting.prettier.with({
+			extra_filetypes = { "toml" },
+			extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+		}),
+		formatting.black.with({ extra_args = { "--fast" } }),
+		-- formatting.codespell,
+		formatting.isort,
+		formatting.stylua,
+		formatting.npm_groovy_lint,
+	},
+})
