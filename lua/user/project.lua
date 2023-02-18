9436ebf6316b1944a9cc5063cb15463daeb678bc
Robin Kautz <git@bob3000.net>
2023-02-18 14:32:59 +0100

    follow focused buffer

--- lua/user/project.lua
+++ lua/user/project.lua
@@ -3,14 +3,20 @@ if not status_ok then
 	return
 end
 project.setup({
-	manual_mode = true,
+  sync_root_with_cwd = true,
+    respect_buf_cwd = true,
+    update_focused_file = {
+      enable = true,
+      update_root = true
+    },
+	manual_mode = false,
+	silent_chdir = false,
 
 	-- detection_methods = { "lsp", "pattern" }, -- NOTE: lsp detection will get annoying with multiple langs in one project
 	detection_methods = { "pattern" },
 
 	-- patterns used to detect root dir, when **"pattern"** is in detection_methods
-	patterns = { ".git", "Makefile", "package.json" },
-	silent_chdir = false,
+	patterns = { ".git", "Makefile", "package.json", "Cargo.toml" },
 })
 
 local tele_status_ok, telescope = pcall(require, "telescope")
