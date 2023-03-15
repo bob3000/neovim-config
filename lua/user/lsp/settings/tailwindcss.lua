b6b81c2484530d18dd49da71f597988d946e5a41
Robin Kautz <git@bob3000.net>
2023-03-15 21:12:31 +0100

    configure tailwindcss lsp
    configure tailwindcss lsp

---
+++ lua/user/lsp/settings/tailwindcss.lua
@@ -0,0 +1,17 @@
+local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
+if not lspconfig_status_ok then
+	return
+end
+local util = lspconfig.util
+
+return {
+	root_dir = function(fname)
+    return util.root_pattern("tailwind.config.ts")(fname)
+      or util.root_pattern("tailwind.config.js")(fname)
+      or util.root_pattern("tailwind.config.cjs")(fname)
+  end,
+	init_options = {
+		lint = true,
+	},
+}
+
