b1f30697725877eb06b9635727af27246a87e70d
Robin Kautz <git@bob3000.net>
2023-10-14 19:46:19 +0200

    move outline to own module

---
+++ lua/user/outline.lua
@@ -0,0 +1,34 @@
+local status_ok, outline = pcall(require, "symbols-outline")
+if not status_ok then
+  return
+end
+
+outline.setup({
+  highlight_hovered_item = true,
+  show_guides = true,
+  auto_preview = false,
+  position = 'right',
+  relative_width = true,
+  width = 25,
+  auto_close = true,
+  show_numbers = false,
+  show_relative_numbers = false,
+  show_symbol_details = true,
+  preview_bg_highlight = 'Pmenu',
+  autofold_depth = 0,
+  auto_unfold_hover = true,
+  fold_markers = { '', '' },
+  wrap = false,
+  keymaps = { -- These keymaps can be a string or a table for multiple keys
+    close = {"<Esc>", "q"},
+    goto_location = "<Cr>",
+    focus_location = "o",
+    hover_symbol = "<C-space>",
+    toggle_preview = "K",
+    rename_symbol = "r",
+    code_actions = "a",
+    fold = "h",
+    unfold = "l",
+    fold_reset = "R",
+  }
+})
