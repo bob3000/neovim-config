67fcc2b10ca1d19ec2fae847a247c5677c109037
Robin Kautz <git@bob3000.net>
2023-10-30 19:13:20 +0100

    use trouble quickfix with git-conflict

--- lua/user/gitconflict.lua
+++ lua/user/gitconflict.lua
@@ -4,12 +4,19 @@ if not status_ok then
 end
 
 local trouble_ok, trouble = pcall(require, "trouble")
-if not trouble_ok then
+if trouble_ok then
   conflict.setup {
     default_mappings = true, -- disable buffer local mapping created by this plugin
     default_commands = true, -- disable commands created by this plugin
     disable_diagnostics = false, -- This will disable the diagnostics in a buffer whilst it is conflicted
-    list_opener = trouble.open "quickfix", -- command or function to open the conflicts list
+    list_opener = function() -- command or function to open the conflicts list
+      local skip_filetypes = {
+        "alpha",
+      }
+      if skip_filetypes[vim.bo.filetype] == nil then
+        trouble.open "quickfix"
+      end
+    end,
     highlights = { -- They must have background color, otherwise the default color will be used
       incoming = "DiffAdd",
       current = "DiffText",
